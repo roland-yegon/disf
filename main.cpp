9a689a2a40f3646c460648175378b36eedb69d39
roland <rolandyegon@gmail.com>
2026-08-31 00:57:39 +0300

    Added the help argument

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+
+void help();
 
 int main(int argc, char *args[]) {
+  // Ensuaring the user passes in a file
   if (argc < 2) {
-    std::cout << "Invalid number arguments\n";
-    std::cout << "USAGE: disf <file_name>\n";
+    std::cout << "Invalid number of arguments\n";
+    std::cout << '\n';
+    help();
     return 1;
   }
 
-  for (int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) { // Loop to access and print multiple files
     std::ifstream file_name{args[i]};
+
+    // Checking for options and invalid files
     if (!file_name) {
-      std::cout << "Failed to read file " << args[i] << '\n';
+      if (static_cast<std::string>(args[1]).compare("--help") == 0) {
+        help();
+        return 0;
+      }
+
+      std::cout << "Failed to read file \"" << args[i] << "\"\n";
       return 1;
     }
 
+    // displaying the file line by line
     std::cout << "\n=========================================\n";
     std::cout << "Displaying: " << args[i];
     std::cout << "\n=========================================\n";
@@ -27,4 +40,15 @@ int main(int argc, char *args[]) {
   }
 
   return 0;
+}
+
+// help function
+void help() {
+  std::cout << "Disf V0.1.0\n";
+  std::cout << "Usage: disf [OPTIONS]... [FILES]...\n";
+  std::cout << "\n[OPTIONS]\n";
+  std::cout << "\t--help\tdisplay the help menu\n";
+  std::cout << "\n[EXAMPLES]\n";
+  std::cout << "\tdisf --help\n";
+  std::cout << "\tdisf file.txt\n";
 }
