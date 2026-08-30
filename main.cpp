a35ed95923be10d35196f1fd5ad001eac5445773
roland <rolandyegon@gmail.com>
2026-08-30 23:59:22 +0300

    Added a feture to take in more than one file

--- main.cpp
+++ main.cpp
@@ -2,21 +2,28 @@
 #include <iostream>
 
 int main(int argc, char *args[]) {
-  if (argc != 2) {
+  if (argc < 2) {
     std::cout << "Invalid number arguments\n";
     std::cout << "USAGE: disf <file_name>\n";
     return 1;
   }
 
-  std::ifstream file_name{args[1]};
-  if (!file_name) {
-    std::cout << "Failed to read file\n";
-    return 1;
-  }
+  for (int i = 1; i < argc; i++) {
+    std::ifstream file_name{args[i]};
+    if (!file_name) {
+      std::cout << "Failed to read file " << args[i] << '\n';
+      return 1;
+    }
 
-  std::string line;
-  while (std::getline(file_name, line)) {
-    std::cout << line << '\n';
+    std::cout << "\n=========================================\n";
+    std::cout << "Displaying: " << args[i];
+    std::cout << "\n=========================================\n";
+
+    std::cout << '\n';
+    std::string line;
+    while (std::getline(file_name, line)) {
+      std::cout << line << '\n';
+    }
   }
 
   return 0;
